a50c3d7ab031651b5cfaa8caacb9e6e833104698
EnzoDiazDev <enzodiazdev@gmail.com>
2020-12-23 18:08:01 -0300

    faztbot proto2

---
+++ src/suscriptors/ReadyLog.ts
@@ -0,0 +1,9 @@
+import Suscriptor from "../bot/eventbroker/Suscriptor";
+
+export default class ReadyLog extends Suscriptor<"ready"> {
+    public event_type:"ready" = "ready";
+
+    public notified():void {
+        console.log("El bot está listo");
+    }
+}
